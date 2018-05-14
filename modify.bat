bfe2692b756a393b386387c5d3cfe69ef6781abe
Alex <whitexela101@gmail.com>
2018-05-15 09:30:38 +1200

    VBox Modify bat

---
+++ modify.bat
@@ -0,0 +1,21 @@
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemVendor" "HP"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemProduct" "ENVY dv7"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemVersion" "<empty>"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemSerial" "string:%random%"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemSKU" "string:%random%"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemFamily" "<empty>"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemUuid" "d18cce0e-44f9-4a74-808c-ff8ff99889da
+
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiBIOSVendor" "HP"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiBIOSVersion" "string:%random%"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiBIOSReleaseDate" "%SYSdate%"
+
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardVendor" "HP"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardProduct" "ENVY dv7"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardVersion" "string:%random%"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardSerial" "string:%random%"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardAssetTag" "string:%random%"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardLocInChass" "<empty>"
+VBoxManage.exe setextradata "Scam" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardBoardType" "10"
+
+pause
